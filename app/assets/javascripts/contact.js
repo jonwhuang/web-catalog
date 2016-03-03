79392f3d47b635cee176d110d827f69ef234f2fb
Jonathan Huang <jonwhuang@gmail.com>
2016-03-03 09:11:32 -0800

    Change contact page to modal

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,19 +1,1 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
 
-$(document).ready(function(){
-  bindContactListeners();
-})
-
-var bindContactListeners = function(){
-  $('.contact-page').on('click', showContactPage);
-  $('#close').on('click', hideContactPage);
-}
-
-var showContactPage = function(){
-  $('#contact-overlay').show();
-}
-
-var hideContactPage = function(){
-  $('#contact-overlay').hide();
-}
