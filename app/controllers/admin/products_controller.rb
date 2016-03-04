9ae1823fc648378fa3abb1def7b7d2196606a308
Jonathan Huang <jonwhuang@gmail.com>
2016-03-03 18:12:28 -0800

    Fix pagination

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,7 +4,7 @@ class Admin::ProductsController < ApplicationController
   before_filter :verify_admin
 
   def index
-    @products = Product.paginate(:page => params[:page]).order(:id)
+    @products = Product.paginate(:page => params[:page], :per_page => 10).order(:id)
   end
 
   def show
